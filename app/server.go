be568a3bd4fbd7ef4fdc4c17137575a2d1b1dc39
Ivar Mahhonin <ivar.mahhonin@gmail.com>
2022-12-13 01:38:51 +0200

    Reading incoming TCP connections and giving simple PONG reply.

--- app/server.go
+++ app/server.go
@@ -7,25 +7,44 @@ import (
 )
 
 const (
-    CONN_HOST = "localhost"
-    CONN_PORT = "3333"
-    CONN_TYPE = "tcp"
+	CONN_HOST = "localhost"
+	CONN_PORT = "6379"
+	CONN_TYPE = "tcp"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	address := fmt.Sprintf("%s:%s", CONN_HOST, CONN_PORT)
 
-	// Uncomment this block to pass the first stage
-	
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen(CONN_TYPE, address)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-	 	os.Exit(1)
+		fmt.Println(fmt.Sprintf("Failed to bind to port %s", CONN_PORT))
+		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-	fmt.Println("Error accepting connection: ", err.Error())
-	os.Exit(1)
+
+	fmt.Printf("Listening on: %s", address)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		buf := make([]byte, 1024)
+		_, err = conn.Read(buf)
+		if err != nil {
+			fmt.Println("Failed reading from connection:", err)
+			return
+		}
+
+		fmt.Println("Received read: ", string(buf))
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Failed write:", err)
+			return
+		}
+
+		conn.Close()
 	}
 }
